95255b97cb70d3811c04d13d5106d55c0ce180b4
Youcef Madadi <42269089+shirogin@users.noreply.github.com>
2023-07-18 18:13:13 +0100

    JS exercice

---
+++ 6- javascript 2/exercice/exercice.js
@@ -0,0 +1,20 @@
+// Javascript practices
+
+// Section 1: Object Manipulation
+// 1. Create a class representing a movie or TV show with properties like title, genre, release year, and a boolean value indicating if it's a movie or TV show.
+// 2. Write a toString method to display the movie or TV show's information in a readable format (e.g., "Title: Inception, Genre: Science Fiction, Release Year: 2010, Type: Movie"). check : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString#examples
+// 3. Add a method to the movie or TV show object that calculates and returns the number of years since its release.
+
+// Section 2: Array Operations
+// 1. Create an array of movie or TV show objects, each representing a different movie or show with properties like title, genre, and release year.
+// 2. Write a function to find the average release year.
+// 3. Write a function that takes the array and returns a new array with only the years of movies (boolean value indicating it's a movie should be true).
+// 4. Write a function that checks if a given genre exists in the array.
+// 5. Write a function that takes the array of movies and shows and returns an array containing only the titles of TV shows.
+// 6. Write a function that finds all movies in the array with a specific genre.
+
+// Section 3: Basic Manipulations
+// 1. Write a function that takes a movie or TV show title as input and returns the title without spaces and all letters in lowercase.
+//    (e.g., "The Lord of the Rings" should become "thelordoftherings").
+// 2. Write a function that returns the longest title in the arrays.
+// 3. Write a function that takes a movie or TV show title as input and returns the number of words in the title.
